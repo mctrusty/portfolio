f404dec15465385a506c0279fc5fb163c80415ae
Michael J. Cox <mike.jon.cox@gmailcom>
2020-01-20 12:52:18 -0700

    added initial markdownx functionality

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    body = models.TextField()
+    body = MarkdownxField()
     image = models.ImageField(upload_to='images/', blank=True)
     pub_date = models.DateField()
     last_modified = models.DateTimeField(auto_now=True)
@@ -17,5 +19,9 @@ class Blog(models.Model):
     def summary(self):
         return self.body[:100]
 
+    @property
+    def formatted_markdown(self):
+        return markdownify(self.body)
+    
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
